506c688fca6ef0a11ed99b8d26460f38c4c457f8
Scott Will <scott.d.will@nasa.gov>
2024-07-16 14:42:52 -0400

    Add AdamOptimizer and TrainingData to top-level namespace
    They are in single-class modules, so it makes sense to just have them
    importable right from corsid

--- corsid/__init__.py
+++ corsid/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 import matplotlib
+from .TrainingData import TrainingData
+from .adam import AdamOptimizer
+
 REPO_ROOT = Path(__file__).parents[1]
 matplotlib.rc_file(REPO_ROOT / 'matplotlibrc')
